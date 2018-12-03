be2481305f85525327affcf98c9834e11441d0ef
Balint Mogyorosi <mogyorosibalin@gmail.com>
2018-12-03 12:08:06 +0100

    Getting programs with rest contoller

--- src/main/java/com/bgm/workoutzback/api/ProgramRestController.java
+++ src/main/java/com/bgm/workoutzback/api/ProgramRestController.java
@@ -1,7 +1,28 @@
 package com.bgm.workoutzback.api;
 
+import com.bgm.workoutzback.model.Program;
+import com.bgm.workoutzback.repository.ProgramRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class ProgramRestController {
+
+    @Autowired
+    private ProgramRepository programRepository;
+
+    @GetMapping("/programs")
+    public List<Program> getPrograms() {
+        return programRepository.findAll();
+    }
+
+    @GetMapping("/programs/{id}")
+    public Program getProgram(@PathVariable("id") long id) {
+        return programRepository.findById(id).orElse(null);
+    }
+
 }
--- src/main/java/com/bgm/workoutzback/repository/ProgramRepository.java
+++ src/main/java/com/bgm/workoutzback/repository/ProgramRepository.java
@@ -4,6 +4,11 @@ import com.bgm.workoutzback.model.Program;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ProgramRepository extends JpaRepository<Program, Long> {
+
+    Optional<Program> findById(long id);
+
 }
