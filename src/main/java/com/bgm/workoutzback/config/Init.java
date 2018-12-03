74406b7c75c22ef2dcea9d8227c4a09cc64cac65
Balint Mogyorosi <mogyorosibalin@gmail.com>
2018-12-03 12:11:35 +0100

    Added program initialization to the project

--- src/main/java/com/bgm/workoutzback/config/Init.java
+++ src/main/java/com/bgm/workoutzback/config/Init.java
@@ -1,7 +1,9 @@
 package com.bgm.workoutzback.config;
 
 import com.bgm.workoutzback.model.Exercise;
+import com.bgm.workoutzback.model.Program;
 import com.bgm.workoutzback.repository.ExerciseRepository;
+import com.bgm.workoutzback.repository.ProgramRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
@@ -13,12 +15,15 @@ public class Init {
 
     @Autowired
     private ExerciseRepository exerciseRepository;
+    @Autowired
+    private ProgramRepository programRepository;
 
     @Bean
     @Order(1)
     public CommandLineRunner initDb() {
         return args -> {
             this.initExercises();
+            this.initPrograms();
         };
     }
 
@@ -26,6 +31,12 @@ public class Init {
         exerciseRepository.save(new Exercise("Push Up", "http://"));
         exerciseRepository.save(new Exercise("Pull Up", "http://"));
         exerciseRepository.save(new Exercise("Dip", "http://"));
+    }
+
+    private void initPrograms() {
+        programRepository.save(new Program("Beginner Program"));
+        programRepository.save(new Program("Beginner Intermediate Program"));
+        programRepository.save(new Program("Intermediate Program"));
     }
 
 }
